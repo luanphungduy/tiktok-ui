f82c84f9340042b0fb99ee88ef7a89e2ba67dfe0
luan <luanphungduy@gmail.com>
2023-09-30 19:40:01 +0700

    105. Xây dựng Logic phần Header #3

---
+++ src/Hooks/useDebounce.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+
+function useDebounce(value, delay) {
+    const [debouncedvalue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+        const handler = setTimeout(() => setDebouncedValue(value), delay);
+
+        return () => clearTimeout(handler);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [value]);
+
+    return debouncedvalue;
+}
+
+export default useDebounce;
